2637d309d4b329fb95752d7b0a4f073b786e1027
Aris Kostakos <a.kostakos@thebeat.co>
2020-03-30 12:37:36 +0300

    added grow poly functions

--- pkg/geo/geo.go
+++ pkg/geo/geo.go
@@ -1,8 +1,13 @@
 package geo
 
 import (
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/golang/geo/r3"
 	"github.com/golang/geo/s2"
-	"github.com/paulmach/go.geojson"
+	geojson "github.com/paulmach/go.geojson"
 )
 
 const (
@@ -76,4 +81,176 @@ func edgesOfCell(c s2.Cell) [][]float64 {
 		edges = append(edges, []float64{latLng.Lat.Degrees(), latLng.Lng.Degrees()})
 	}
 	return edges
+}
+
+// RawPoint is Polygon Points (lat,long).
+type RawPoint = [][]float64
+
+// DistanceFunc accepts two points and returns a distance as float64.
+type DistanceFunc func(p1, p2 Point) float64
+
+// CalculateEquirectangularDistance is a DistanceFunc calculating distance with the equirectangular formula.
+func CalculateEquirectangularDistance(p1, p2 Point) float64 {
+	x := toRadians(p2.Lng-p1.Lng) * math.Cos(toRadians(p1.Lat+p2.Lat)/2)
+	y := toRadians(p2.Lat - p1.Lat)
+	return math.Sqrt(x*x+y*y) * EarthRadius
+}
+
+func toRadians(v float64) float64 {
+	return v * (math.Pi / 180.0)
+}
+
+// GeoJSONPointsToPolygon converts geo json points (lng,lat) to s2 Polygon.
+func GeoJSONPointsToPolygon(points []RawPoint) []*s2.Polygon {
+	s2Polygons := []*s2.Polygon{}
+	for _, p := range points {
+		s2p := parseGeoJSONPointsToS2Polygon(p)
+		s2Polygons = append(s2Polygons, s2p)
+	}
+	return s2Polygons
+}
+
+func parseGeoJSONPointsToS2Polygon(points RawPoint) *s2.Polygon {
+	var pts []s2.Point
+	for _, pt := range points {
+		pts = append(pts, s2.PointFromLatLng(s2.LatLngFromDegrees(pt[1], pt[0])))
+	}
+	loop := s2.LoopFromPoints(pts)
+
+	return s2.PolygonFromLoops([]*s2.Loop{loop})
+}
+
+//PolygonContainsPoints checks if one or more points are inside the given polygon
+func PolygonContainsPoints(polygon s2.Polygon, points ...Point) bool {
+	for _, point := range points {
+		p := s2.PointFromLatLng(s2.LatLngFromDegrees(point.Lat, point.Lng))
+		if !polygon.ContainsPoint(p) {
+			return false
+		}
+	}
+	return true
+}
+
+//UnmarshalGeoJSON decodes the data into an array of floats
+func UnmarshalGeoJSON(json []byte) ([]RawPoint, error) {
+	fc, err := geojson.UnmarshalFeatureCollection(json)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseFeatureCollection(fc)
+}
+
+//ParseFeatureCollection decodes a feature collection into an array of floats
+func ParseFeatureCollection(fc *geojson.FeatureCollection) ([]RawPoint, error) {
+	ftrs := []RawPoint{}
+	for _, f := range fc.Features {
+		ftr, err := parseFeature(f)
+		if err != nil {
+			return nil, err
+		}
+		ftrs = append(ftrs, ftr)
+	}
+
+	return ftrs, nil
+}
+
+func parseFeature(f *geojson.Feature) (RawPoint, error) {
+	if f == nil {
+		return nil, errors.New("feature cannot be null")
+	}
+	if !f.Geometry.IsPolygon() {
+		return nil, errors.New("only polygon type supported")
+	}
+
+	if len(f.Geometry.Polygon) == 0 {
+		return nil, errors.New("cannot find the polygon")
+	}
+
+	return f.Geometry.Polygon[0], nil
+}
+
+// IsPointInsidePolygons checks if any of the given polygons contain a given point.
+func IsPointInsidePolygons(loc Point, partnerPolygons []*s2.Polygon) bool {
+	for _, p := range partnerPolygons {
+		if PolygonContainsPoints(*p, loc) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func pToString(p s2.Point) string {
+	return fmt.Sprintf("\t(%0.2f\t%0.2f)", p.X, p.Y)
+}
+
+// GrowPolygon returns a new polygon, scaled with given buffer
+func GrowPolygon(poly *s2.Polygon, buffer float64) (*s2.Polygon, error) {
+	fmt.Println("GOT POLYGON:")
+	loop := poly.Loops()[0]
+	fmt.Println("points:")
+	for i, v := range loop.Vertices() {
+		fmt.Printf("point #%d:%s\n", i, pToString(v))
+	}
+	fmt.Println("edges:")
+	edgeNormals := make([]r3.Vector, loop.NumVertices())
+	for i := 0; i < loop.NumVertices(); i++ {
+		var edge s2.Edge
+		if i == loop.NumVertices()-1 {
+			edge = s2.Edge{loop.Vertices()[i], loop.Vertices()[0]}
+		} else {
+			edge = s2.Edge{loop.Vertices()[i], loop.Vertices()[i+1]}
+		}
+		fmt.Printf("edge #%d: %s->%s\n", i, pToString(edge.V0), pToString(edge.V1))
+
+		midPoint := s2.Point{edgeMidpoint(edge)}
+		n1, _ := edgeNormal(edge)
+		n1Norm := n1.Normalize()
+		edgeNormals[i] = n1Norm
+		_ = midPoint
+		//fmt.Printf("center: %s\n", pToString(midPoint))
+		fmt.Printf("normals: %s\n", pToString(s2.Point{n1Norm}))
+		//fmt.Printf("center*normal: %s\n", pToString(s2.Point{midPoint.Add(n1Norm.Mul(buffer))}))
+	}
+
+	for i := 0; i < loop.NumVertices(); i++ {
+		var edgeIdA, edgeIdB int
+
+		if i == 0 {
+			edgeIdA = loop.NumVertices() - 1
+		} else {
+			edgeIdA = i - 1
+		}
+
+		edgeIdB = i
+
+		vec := loop.Vertices()[i]
+		na := edgeNormals[edgeIdA]
+		nb := edgeNormals[edgeIdB]
+
+		bis := na.Add(nb).Normalize()
+
+		l := buffer / math.Sqrt(1+na.Dot(nb))
+
+		offsetVec := r3.Vector{vec.X + l*bis.X, vec.Y + l*bis.Y, 0}
+		//bis = bis
+		//fmt.Printf("Vertex #%d bis: %s\n", i, pToString(s2.Point{bis}))
+		fmt.Printf("Vertex #%d new: %s\n", i, pToString(s2.Point{offsetVec}))
+	}
+
+	fmt.Println("\nMADE NEW GROWN POLYGON:")
+	grownPoly := &s2.Polygon{}
+
+	return grownPoly, nil
+}
+
+func edgeNormal(e s2.Edge) (r3.Vector, r3.Vector) {
+	dx := e.V1.X - e.V0.X
+	dy := e.V1.Y - e.V0.Y
+	return r3.Vector{-dy, dx, 0}, r3.Vector{dy, -dx, 0}
+}
+
+func edgeMidpoint(e s2.Edge) r3.Vector {
+	return r3.Vector{(e.V0.X + e.V1.X) / 2, (e.V0.Y + e.V1.Y) / 2, 0}
 }
--- pkg/geo/geo_test.go
+++ pkg/geo/geo_test.go
@@ -1,8 +1,11 @@
 package geo
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/golang/geo/r3"
+	"github.com/golang/geo/s2"
+	"github.com/stretchr/testify/assert"
 )
 
 var validJSON = []byte(`
@@ -75,4 +78,299 @@ func TestCoverPoint(t *testing.T) {
 	assert.Equal(t, "14", token)
 	assert.Equal(t, 4, len(edges[0]))
 
+}
+
+var l1 = []RawPoint{{
+	{23.823165893554688, 38.036734877267705},
+	{23.661460876464844, 38.031867399480674},
+	{23.5821533203125, 37.915763487770754},
+	{23.809089660644528, 37.90113599940821},
+	{23.823165893554688, 38.036734877267705},
+},
+}
+
+var squarePolyPoints = []s2.Point{
+	s2.Point{r3.Vector{-50, 50, 0}},
+	s2.Point{r3.Vector{50, 50, 0}},
+	s2.Point{r3.Vector{50, -50, 0}},
+	s2.Point{r3.Vector{-50, -50, 0}},
+}
+
+func TestGrowPolygon(t *testing.T) {
+	loop := s2.LoopFromPoints(squarePolyPoints)
+	polygon := s2.PolygonFromLoops([]*s2.Loop{loop})
+	GrowPolygon(polygon, 10)
+	t.Error()
+}
+func TestGeoJsonPointsToPolygon(t *testing.T) {
+	p := GeoJSONPointsToPolygon(l1)
+	assert.False(t, p[0].IsEmpty())
+}
+
+func TestPolygonContainsPoints(t *testing.T) {
+	p := GeoJSONPointsToPolygon(l1)
+	p1 := Point{Lat: 38.00346733273856, Lng: 23.79329681396484}
+	p2 := Point{Lat: 37.98317483351337, Lng: 23.755874633789062}
+	actual := PolygonContainsPoints(*p[0], p1, p2)
+	assert.True(t, actual)
+
+	p3 := Point{Lat: 38.002385207833235, Lng: 23.854751586914062}
+	actual = PolygonContainsPoints(*p[0], p1, p3)
+	assert.False(t, actual)
+}
+
+func TestUnmarshalSinglePolygonGeoJSON(t *testing.T) {
+	p, err := UnmarshalGeoJSON(getSinglePolyGeoJSON())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, p)
+	assert.Len(t, p, 1)
+	assert.Len(t, p[0], 5)
+	assert.Equal(t, l1, p)
+
+	p, err = UnmarshalGeoJSON([]byte("foo"))
+	assert.Error(t, err)
+
+	p, err = UnmarshalGeoJSON(getPointGeoJSON())
+	assert.Error(t, err)
+}
+
+func TestUnmarshalMultiPolygonGeoJSON(t *testing.T) {
+	var expPoly = []RawPoint{{
+		{-70.74440002441406, -33.442328356136564},
+		{-70.74920654296875, -33.47125826950989},
+		{-70.70148468017578, -33.47211742624329},
+		{-70.70663452148438, -33.43258740206331},
+		{-70.74440002441406, -33.442328356136564},
+	}, {
+		{-70.67367553710938, -33.42399153452155},
+		{-70.68260192871094, -33.457510603405886},
+		{-70.65067291259766, -33.42112605618061},
+		{-70.67367553710938, -33.42399153452155},
+	},
+	}
+	m, err := UnmarshalGeoJSON(getMultiPolyGeoJSON())
+	assert.NoError(t, err)
+	assert.Len(t, m, 2)
+	assert.Len(t, m[0], 5)
+	assert.Len(t, m[1], 4)
+	assert.Equal(t, expPoly, m)
+
+	m, err = UnmarshalGeoJSON(getSecondPointGeoJSON())
+	assert.Error(t, err)
+}
+
+func getSinglePolyGeoJSON() []byte {
+	return []byte(`
+  {
+"type": "FeatureCollection",
+"features": [
+  {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "Polygon",
+      "coordinates": [
+        [
+          [
+            23.823165893554688,
+            38.036734877267705
+          ],
+          [
+            23.661460876464844,
+            38.031867399480674
+          ],
+          [
+            23.5821533203125,
+            37.915763487770754
+          ],
+          [
+            23.809089660644528,
+            37.90113599940821
+          ],
+          [
+            23.823165893554688,
+            38.036734877267705
+          ]
+        ]
+      ]
+    }
+  }
+]
+}`)
+}
+
+func getMultiPolyGeoJSON() []byte {
+	return []byte(`
+  {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -70.74440002441406,
+                -33.442328356136564
+              ],
+              [
+                -70.74920654296875,
+                -33.47125826950989
+              ],
+              [
+                -70.70148468017578,
+                -33.47211742624329
+              ],
+              [
+                -70.70663452148438,
+                -33.43258740206331
+              ],
+              [
+                -70.74440002441406,
+                -33.442328356136564
+              ]
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -70.67367553710938,
+                -33.42399153452155
+              ],
+              [
+                -70.68260192871094,
+                -33.457510603405886
+              ],
+              [
+                -70.65067291259766,
+                -33.42112605618061
+              ],
+              [
+                -70.67367553710938,
+                -33.42399153452155
+              ]
+            ]
+          ]
+        }
+      }
+    ]
+  }
+  `)
+}
+
+func getPointGeoJSON() []byte {
+	return []byte(`
+  {
+"type": "FeatureCollection",
+"features": [
+  {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        23.784713745117188,
+        38.0091482264894
+      ]
+    }
+  }
+]
+}`)
+}
+
+func getSecondPointGeoJSON() []byte {
+	return []byte(`
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -70.74440002441406,
+              -33.442328356136564
+            ],
+            [
+              -70.74920654296875,
+              -33.47125826950989
+            ],
+            [
+              -70.70148468017578,
+              -33.47211742624329
+            ],
+            [
+              -70.70663452148438,
+              -33.43258740206331
+            ],
+            [
+              -70.74440002441406,
+              -33.442328356136564
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          [
+            [
+              -70.67367553710938,
+              -33.42399153452155
+            ],
+            [
+              -70.68260192871094,
+              -33.457510603405886
+            ],
+            [
+              -70.65067291259766,
+              -33.42112605618061
+            ],
+            [
+              -70.67367553710938,
+              -33.42399153452155
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
+`)
+}
+
+func getNoFeaturesGeoJSON() []byte {
+	return []byte(`
+  {
+"type": "FeatureCollection",
+"invalid": [
+  {
+    "type": "Polygon",
+    "properties": {},
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        23.784713745117188,
+        38.0091482264894
+      ]
+    }
+  }
+]
+}`)
 }
